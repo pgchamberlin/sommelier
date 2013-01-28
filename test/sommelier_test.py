efe6b8f81395a1e1cc5bfd7d0c1094ae5938e331
pgchamberlin <pete@f-box.org>
2013-01-28 22:03:36 +0000

    HelloWorlded scipy

--- test/sommelier_test.py
+++ test/sommelier_test.py
@@ -1,11 +1,16 @@
 import unittest
+import scipy.stats.stats
 
 class TestSommelier(unittest.TestCase):
 
   def setUp(self):
-    self.name = 'sommelier'
+    self.people = ['bruce','carl','derek']
+    self.people['bruce'] = [1,2,3,4]
+    self.people['carl']  = [2,3,4,5]
+    self.people['derek'] = [4,0,9,1]
   
   def test_name(self):
+    scipy.stats.pearsonr(self.people['bruce'],self.people['carl']);
     self.assertEqual(self.name, 'sommelier')
   
 if __name__ == '__main__':
