14af3a26b2f78779ffeea5207f3d77ef22874f3c
pgchamberlin <pete@f-box.org>
2013-02-25 15:12:56 +0000

    More work in recommendations.py based around the tutorial in Segaran Ch.2

--- lib/recommendations.py
+++ lib/recommendations.py
@@ -85,3 +85,40 @@ def euclidean_distance(prefs,person1,person2):
 
   return 1/(1+euclidean_distance)
 
+# Method copied from Segaran: Collective Intelligence (2006) Ch.2
+def sim_pearson(prefs,p1,p2):
+  si={}
+  for item in prefs[p1]:
+    if item in prefs[p2]: si[item]=1
+
+  n=len(si)
+
+  if n==0: return 0
+
+  sum1=sum([prefs[p1][it] for it in si])
+  sum2=sum([prefs[p2][it] for it in si])
+
+  sum1Sq=sum([pow(prefs[p1][it],2) for it in si])
+  sum2Sq=sum([pow(prefs[p2][it],2) for it in si])
+
+  pSum=sum([prefs[p1][it]*prefs[p2][it] for it in si])
+
+  # calculate Pearson score:
+  num=pSum-(sum1*sum2/n)
+  den=sqrt((sum1Sq-pow(sum1,2)/n)*(sum2Sq-pow(sum2,2)/n))
+
+  if den==0: return 0
+
+  r=num/den
+
+  return r
+
+# Method copied from Segaran: Collective Intelligence (2006) Ch.2
+def topMatches(prefs,person,n=5,similarity=sim_pearson):
+  scores=[(similarity(prefs,person,other), other)
+      for other in prefs if other!=person]
+
+  scores.sort()
+  scores.reverse()
+  return scores[0:n]
+
