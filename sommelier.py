a84a311746e67d2adb458ed4a6720d60bbd51f9c
pgchamberlin <pete@f-box.org>
2013-03-10 19:38:16 +0000

    Updated routes for sommelier app

--- sommelier.py
+++ sommelier.py
@@ -4,7 +4,7 @@ from flask import Flask, request, Response, jsonify, json
 app = Flask(__name__)
 broker = WineBroker()
 
-def get_sommelier_response(name, content, meta={}):
+def sommelier_response(name, content, meta={}):
     response = json.dumps({ name: content, 'meta': meta })
     return Response(response.decode('unicode-escape'), status=200, mimetype='application/json; charset=utf-8')
 
@@ -12,20 +12,12 @@ def get_sommelier_response(name, content, meta={}):
 def sommelier_index():
     return "index"
 
-@app.route('/authenticate', methods = ['GET','POST'])
-def sommelier_authenticate():
-    return 'authenticate'
-
-@app.route('/deauthenticate', methods = ['GET'])
-def sommelier_deauthenticate():
-    return 'deauthenticate'
-
 @app.route('/wines', defaults = {'page': 1}, methods = ['GET'])
 @app.route('/wines/<int:page>', methods = ['GET'])
 def sommelier_wines(page):
     records = broker.getPage(page)
     numpages = broker.getNumPages()
-    return get_sommelier_response('wines', records, { 'numpages': numpages })
+    return sommelier_response('wines', records, { 'numpages': numpages })
 
 @app.route('/wine/<wineid>', methods = ['GET','POST'])
 def sommelier_wine(wineid):
@@ -36,7 +28,19 @@ def sommelier_wine(wineid):
 def sommelier_users():
     return 'list of users'
 
-@app.route('/useri/<username>', methods = ['GET','POST'])
+@app.route('/user/new', methods = ['GET', 'POST'])
+def sommelier_users():
+    return 'list of users'
+
+@app.route('/user/authenticate', methods = ['GET','POST'])
+def sommelier_authenticate():
+    return 'authenticate user'
+
+@app.route('/user/deauthenticate', methods = ['GET'])
+def sommelier_deauthenticate():
+    return 'deauthenticate user'
+
+@app.route('/user/<username>', methods = ['GET','POST'])
 def sommelier_user():
     return 'individual user: %s' % username
 
