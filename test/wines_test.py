499aced0d148ab79a22bfe143650cbd6ae65f471
pgchamberlin <pete@f-box.org>
2013-03-10 19:40:17 +0000

    fixed wines test

--- test/wines_test.py
+++ test/wines_test.py
@@ -1,6 +1,5 @@
 #!python
 import unittest
-import pprint
 from mock import Mock, MagicMock
 from wines import WineBroker
 
@@ -16,19 +15,19 @@ class WinesTest(unittest.TestCase):
         page = self.winebroker.getPage(1)
         expectedquery = "SELECT * FROM sommelier LIMIT 50 OFFSET 0"
         self.winebroker.db.execute.assert_called_once_with(expectedquery)
-        self.winebroker.db.fetchall.assert_called_once()
+        self.winebroker.db.fetchall.assert_called()
     
     def testGetNumPages(self):
         numpages = self.winebroker.getNumPages()
         expectedquery = "SELECT COUNT(*) FROM sommelier"
         self.winebroker.db.execute.assert_called_once_with(expectedquery)
-        self.winebroker.db.fetchone.assert_called_once()
+        self.winebroker.db.fetchone.assert_called()
     
     def testGetWine(self):
         wine = self.winebroker.getWine(123)
         expectedquery = "SELECT * FROM sommelier WHERE id = 123"
         self.winebroker.db.execute.assert_called_once_with(expectedquery)
-        self.winebroker.db.fetchone.assert_called_once()
+        self.winebroker.db.fetchone.assert_called()
 
 if __name__ == '__main__':
     unittest.main()
