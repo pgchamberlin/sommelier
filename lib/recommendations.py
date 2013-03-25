ea1150204dffdcc33e81d21e8cfede15928f6c27
pgchamberlin <pete@f-box.org>
2013-03-25 19:41:19 +0000

    New methods in recommendations.py

--- lib/recommendations.py
+++ lib/recommendations.py
@@ -2,216 +2,245 @@
 # set of movies
 # copied from Segaran: Collective Intelligence (2006) Ch.2
 critics={
-  'Lisa Rose': {
-    'Lady in the Water': 2.5,
-    'Snakes on a Plane': 3.5,
-    'Just My Luck': 3.0,
-    'Superman Returns': 3.5,
-    'You, Me and Dupree': 2.5,
-    'The Night Listener': 3.0
-  },
-  'Gene Seymour': {
-    'Lady in the Water': 3.0,
-    'Snakes on a Plane': 3.5,
-    'Just My Luck': 1.5,
-    'Superman Returns': 5.0,
-    'You, Me and Dupree': 3.5,
-    'The Night Listener': 3.0
-  },
-  'Michael Phillips': {
-    'Lady in the Water': 2.5,
-    'Snakes on a Plane': 3.5,
-    'Superman Returns': 3.5,
-    'The Night Listener': 4.0
-  },
-  'Claudia Puig': {
-    'Snakes on a Plane': 3.5,
-    'Just My Luck': 3.0,
-    'Superman Returns': 4.0,
-    'You, Me and Dupree': 2.5,
-    'The Night Listener': 4.5
-  },
-  'Mike LaSalle': {
-    'Lady in the Water': 3.0,
-    'Snakes on a Plane': 4.0,
-    'Just My Luck': 2.0,
-    'Superman Returns': 3.0,
-    'You, Me and Dupree': 2.0,
-    'The Night Listener': 3.0
-  },
-  'Jack Matthews': {
-    'Lady in the Water': 3.0,
-    'Snakes on a Plane': 4.0,
-    'Superman Returns': 5.0,
-    'You, Me and Dupree': 3.5,
-    'The Night Listener': 3.0
-  },
-  'Toby': {
-    'Snakes on a Plane': 4.5,
-    'Superman Returns': 4.0,
-    'You, Me and Dupree': 1.0
-  }
+    'Lisa Rose': {
+        'Lady in the Water': 2.5,
+        'Snakes on a Plane': 3.5,
+        'Just My Luck': 3.0,
+        'Superman Returns': 3.5,
+        'You, Me and Dupree': 2.5,
+        'The Night Listener': 3.0
+    },
+    'Gene Seymour': {
+        'Lady in the Water': 3.0,
+        'Snakes on a Plane': 3.5,
+        'Just My Luck': 1.5,
+        'Superman Returns': 5.0,
+        'You, Me and Dupree': 3.5,
+        'The Night Listener': 3.0
+    },
+    'Michael Phillips': {
+        'Lady in the Water': 2.5,
+        'Snakes on a Plane': 3.5,
+        'Superman Returns': 3.5,
+        'The Night Listener': 4.0
+    },
+    'Claudia Puig': {
+        'Snakes on a Plane': 3.5,
+        'Just My Luck': 3.0,
+        'Superman Returns': 4.0,
+        'You, Me and Dupree': 2.5,
+        'The Night Listener': 4.5
+    },
+    'Mike LaSalle': {
+        'Lady in the Water': 3.0,
+        'Snakes on a Plane': 4.0,
+        'Just My Luck': 2.0,
+        'Superman Returns': 3.0,
+        'You, Me and Dupree': 2.0,
+        'The Night Listener': 3.0
+    },
+    'Jack Matthews': {
+        'Lady in the Water': 3.0,
+        'Snakes on a Plane': 4.0,
+        'Superman Returns': 5.0,
+        'You, Me and Dupree': 3.5,
+        'The Night Listener': 3.0
+    },
+    'Toby': {
+        'Snakes on a Plane': 4.5,
+        'Superman Returns': 4.0,
+        'You, Me and Dupree': 1.0
+    }
 }
 
 # Method copied from Segaran: Collective Intelligence (2006) Ch.2
 from math import sqrt
 def sim_distance(prefs,person1,person2):
-  si={}
-  for item in prefs[person1]:
-    if item in prefs[person2]:
-      si[item]=1
+    si={}
+    for item in prefs[person1]:
+        if item in prefs[person2]:
+            si[item]=1
 
-  if len(si)==0: return 0
+    if len(si)==0: return 0
 
-  sum_of_squares=sum([pow(prefs[person1][item]-prefs[person2][item],2)
-                      for item in prefs[person1] if item in prefs[person2]])
+    sum_of_squares=sum([pow(prefs[person1][item]-prefs[person2][item],2)
+                                            for item in prefs[person1] if item in prefs[person2]])
 
-  return 1/(1+sum_of_squares)
+    return 1/(1+sum_of_squares)
 
 # This method is equivalent to sim_distance() above, uses scipy's sqeuclidean method
 import scipy.spatial
 def euclidean_distance(prefs,person1,person2):
-  vector1=[]
-  vector2=[]
-  for item in prefs[person1]:
-    if item in prefs[person2]:
-      vector1.append(prefs[person1][item])
-      vector2.append(prefs[person2][item])
+    vector1=[]
+    vector2=[]
+    for item in prefs[person1]:
+        if item in prefs[person2]:
+            vector1.append(prefs[person1][item])
+            vector2.append(prefs[person2][item])
 
-  if len(vector1)==0: return 0
+    if len(vector1)==0: return 0
 
-  euclidean_distance=scipy.spatial.distance.sqeuclidean(vector1,vector2)
+    euclidean_distance=scipy.spatial.distance.sqeuclidean(vector1,vector2)
 
-  return 1/(1+euclidean_distance)
+    return 1/(1+euclidean_distance)
 
 # Method copied from Segaran: Collective Intelligence (2006) Ch.2
 def sim_pearson(prefs,p1,p2):
-  si={}
-  for item in prefs[p1]:
-    if item in prefs[p2]: si[item]=1
+    si={}
+    for item in prefs[p1]:
+        if item in prefs[p2]: si[item]=1
 
-  n=len(si)
+    n=len(si)
 
-  if n==0: return 0
+    if n==0: return 0
 
-  sum1=sum([prefs[p1][it] for it in si])
-  sum2=sum([prefs[p2][it] for it in si])
+    sum1=sum([prefs[p1][it] for it in si])
+    sum2=sum([prefs[p2][it] for it in si])
 
-  sum1Sq=sum([pow(prefs[p1][it],2) for it in si])
-  sum2Sq=sum([pow(prefs[p2][it],2) for it in si])
+    sum1Sq=sum([pow(prefs[p1][it],2) for it in si])
+    sum2Sq=sum([pow(prefs[p2][it],2) for it in si])
 
-  pSum=sum([prefs[p1][it]*prefs[p2][it] for it in si])
+    pSum=sum([prefs[p1][it]*prefs[p2][it] for it in si])
 
-  # calculate Pearson score:
-  num=pSum-(sum1*sum2/n)
-  den=sqrt((sum1Sq-pow(sum1,2)/n)*(sum2Sq-pow(sum2,2)/n))
+    # calculate Pearson score:
+    num=pSum-(sum1*sum2/n)
+    den=sqrt((sum1Sq-pow(sum1,2)/n)*(sum2Sq-pow(sum2,2)/n))
 
-  if den==0: return 0
+    if den==0: return 0
 
-  r=num/den
+    r=num/den
 
-  return r
+    return r
 
 # Method copied from Segaran: Collective Intelligence (2006) Ch.2
 def topMatches(prefs,person,n=5,similarity=sim_pearson):
-  scores=[(similarity(prefs,person,other), other)
-      for other in prefs if other!=person]
+    scores=[(similarity(prefs,person,other), other)
+            for other in prefs if other!=person]
 
-  scores.sort()
-  scores.reverse()
-  return scores[0:n]
+    scores.sort()
+    scores.reverse()
+    return scores[0:n]
 
 # Method copied from Segaran: Collective Intelligence (2006) Ch.2
 # Gets recommendations for a person by using weighted average
 # of every other user's rankings
 def getRecommendations(prefs,person,similarity=sim_pearson):
-  totals={}
-  simSums={}
-  for other in prefs:
-    if other==person: continue
-    sim=similarity(prefs,person,other)
+    totals={}
+    simSums={}
+    for other in prefs:
+        if other==person: continue
+        sim=similarity(prefs,person,other)
 
-    if sim<=0: continue
-    for item in prefs[other]:
+        if sim<=0: continue
+        for item in prefs[other]:
 
-      # only score movies 'person' hasn't seen
-      if item not in prefs[person] or prefs[person][item]==0:
-        # similarity*score
-        totals.setdefault(item,0)
-        totals[item]+=prefs[other][item]*sim
+            # only score movies 'person' hasn't seen
+            if item not in prefs[person] or prefs[person][item]==0:
+                # similarity*score
+                totals.setdefault(item,0)
+                totals[item]+=prefs[other][item]*sim
 
-        # sum of similarities
-        simSums.setdefault(item,0)
-        simSums[item]+=sim
+                # sum of similarities
+                simSums.setdefault(item,0)
+                simSums[item]+=sim
 
-  rankings=[(total/simSums[item],item) for item,total in totals.items()]
+    rankings=[(total/simSums[item],item) for item,total in totals.items()]
 
-  rankings.sort()
-  rankings.reverse()
-  return rankings
+    rankings.sort()
+    rankings.reverse()
+    return rankings
 
 # Method copied from Segaran: Collective Intelligence (2006) Ch.2
 def transformPrefs(prefs):
-  result={}
-  for person in prefs:
-    for item in prefs[person]:
-      result.setdefault(item,{})
-      result[item][person]=prefs[person][item]
-  return result
+    result={}
+    for person in prefs:
+        for item in prefs[person]:
+            result.setdefault(item,{})
+            result[item][person]=prefs[person][item]
+    return result
 
 # Method copied from Segaran: Collective Intelligence (2006) Ch.2
 def calculateSimilarItems(prefs,n=10):
-  result={}
+    result={}
 
-  itemPrefs=transformPrefs(prefs)
-  c=0
-  for item in itemPrefs:
-    c+=1
-    if c%100==0: print "%d / %d" % (c,len(itemPrefs))
-    
-    scores=topMatches(itemPrefs,item,n=n,similarity=sim_distance)
-    result[item]=scores
+    itemPrefs=transformPrefs(prefs)
+    c=0
+    for item in itemPrefs:
+        c+=1
+        if c%100==0: print "%d / %d" % (c,len(itemPrefs))
+        
+        scores=topMatches(itemPrefs,item,n=n,similarity=sim_distance)
+        result[item]=scores
 
-  return result
+    return result
 
 # Method copied from Segaran: Collective Intelligence (2006) Ch.2
 def getRecommendedItems(prefs,itemMatch,user):
-  userRatings=prefs[user]
-  scores={}
-  totalSim={}
+    userRatings=prefs[user]
+    scores={}
+    totalSim={}
 
-  for (item,rating) in userRatings.items():
-    for (similarity, item2) in itemMatch[item]:
-      if item2 in userRatings: continue
+    for (item,rating) in userRatings.items():
+        for (similarity, item2) in itemMatch[item]:
+            if item2 in userRatings: continue
 
-      # Weighted sum of rating times similarity
-      scores.setdefault(item2,0)
-      scores[item2]+=similarity*rating
+            # Weighted sum of rating times similarity
+            scores.setdefault(item2,0)
+            scores[item2]+=similarity*rating
 
-      # Sum of all the similarities
-      totalSim.setdefault(item2,0)
-      totalSim[item2]+=similarity
+            # Sum of all the similarities
+            totalSim.setdefault(item2,0)
+            totalSim[item2]+=similarity
 
-  # Divide each total score by total weighting to give an average
-  rankings=[(score/totalSim[item],item) for item,score in scores.items()]
+    # Divide each total score by total weighting to give an average
+    rankings=[(score/totalSim[item],item) for item,score in scores.items()]
 
-  rankings.sort()
-  rankings.reverse()
-  return rankings
+    rankings.sort()
+    rankings.reverse()
+    return rankings
 
 # Method copied from Segaran: Collective Intelligence (2006) Ch.2
 def loadMovieLens(path='../data/ml-100k'):
 
-  movies={}
-  for line in open(path+'/u.item'):
-    (id,title)=line.split('|')[0:2]
-    movies[id]=title
+    movies={}
+    for line in open(path+'/u.item'):
+        (id,title)=line.split('|')[0:2]
+        movies[id]=title
 
-  prefs={}
-  for line in open(path+'/u.data'):
-    (user,movieid,rating,ts)=line.split('\t')
-    prefs.setdefault(user,{})
-    prefs[user][movies[movieid]]=float(rating)
+    prefs={}
+    for line in open(path+'/u.data'):
+        (user,movieid,rating,ts)=line.split('\t')
+        prefs.setdefault(user,{})
+        prefs[user][movies[movieid]]=float(rating)
 
-  return prefs
+    return prefs
+
+def loadSommelierWines(comparator='rating'):
+
+    from src.sommelier import SommelierDb
+    db = SommelierDb()
+    db.execute("""
+select sw.name as wine, sw.vintage as vintage, t.author as author, t.rating as rating, t.notes as notes from sommelier_wine_complete sw join sommelier_tasting t on t.wine_id = sw.id where 1 < ( select count( distinct t2.author ) from sommelier_tasting t2 where t2.wine_id = sw.id and t.rating <> 0 )
+    """)
+    results = db.fetchall()
+
+    prefs={}
+    for row in results:
+        user = row['author']
+        wine = row['wine']
+        vintage = row['vintage']
+        rating = row['rating']
+        notes = row['notes']
+        prefs.setdefault(user,{})
+        if comparator == 'notes':
+            comp = row['notes']
+        else:
+            comp = row['rating'] + 0.0
+        prefs[user][''.join([wine,str(vintage)])] = comp
+        #     {
+        #        'rating': row['rating'], 
+        #        'notes': row['notes']
+        #     }
+
+    return prefs
 
