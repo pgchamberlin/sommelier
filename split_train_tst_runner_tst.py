27d7b9790fc18dda77a7b119dbc47ec6e8df36ee
pgchamberlin <pete@f-box.org>
2013-05-04 05:28:16 +0100

    Added sommelier db to repo

--- split_train_tst_runner_tst.py
+++
@@ -1,6 +0,0 @@
-#!flask/bin/python
-from src.recommender import SommelierYeungMFRecommender, SommelierRecommender
-y = SommelierYeungMFRecommender()
-y.split_data_evaluation([
-    {"steps":10,  "factors":10, "verbose":True},
-], percent_train=95)
